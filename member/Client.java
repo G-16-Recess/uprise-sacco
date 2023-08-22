0d9dc5ba253be263ae2f6c8a4d3b387d83d8b030
Liven-Al <138493253+Liven-Al@users.noreply.github.com>
2023-08-22 22:05:31 +0300

    Update to allow accept typing(user response)

--- member/Client.java
+++ member/Client.java
@@ -4,7 +4,7 @@ import java.net.*;
 
 public class Client {
     public static String displayMenu() {
-        String menu = "\n\t\tCommands\n-----------------------------------------------\n- deposit <amount> <date_deposited> <receipt_number>\n- CheckStatement <dateFrom> <dateTo>\n- requestLoan <amount> <paymentPeriod_in_months>\n- LoanRequestStatus <loan_application_number>\n- exit\n";
+        String menu = "\n\t\tCommands\n-----------------------------------------------\n- deposit <amount> <date_deposited> <receipt_number>\n- CheckStatement <dateFrom> <dateTo>\n- requestLoan <amount> <paymentPeriod_in_months>\n- LoanRequestStatus <loan_application_number>\n- loanRepayment <application_number> <amount>\n- loanRecord <member_ID>\n- exit\n";
         return menu;
     }
 
@@ -17,26 +17,46 @@ public class Client {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             String userInput;
             System.out.println("Use 'login <username> <password>' to login");
+            
             while ((userInput = in.readLine()) != null) {
                 out.println(userInput);
                 String serverResponse = serverIn.readLine();
+                
                 if (serverResponse.equals("login failed")) {
                     System.out.println("Server response: " + serverResponse + "!. Try again or Use 'forgotPassword <member ID> <phone number>' to recover your password");
                 } else if (serverResponse.equals("Invalid command")) {
                     System.out.println("Server response: " + serverResponse + ". Kindly refer to command menu above");
                 } else if (serverResponse.equals("No records found. Return after a day")) {
+                    System.out.println(serverResponse); 
+                } else if (serverResponse.startsWith("Application number:")) {
+        
+                    StringBuilder loanRecordResponse = new StringBuilder(serverResponse);
+                    while ((serverResponse = serverIn.readLine()) != null && !serverResponse.isEmpty()) {
+                    loanRecordResponse.append("\n").append(serverResponse);
+                }
+                    System.out.println("Server response:\n" + loanRecordResponse.toString());
+                   
+                } else if (serverResponse.startsWith("Congragulations your loan has been approved")) {
                     System.out.println(serverResponse);
+                    String userResponse = in.readLine();
+                  //  System.out.println("User Response: " + userResponse);
+                    out.println(userResponse);
+
+                   if (userResponse.equalsIgnoreCase("accept")) {
+                   StringBuilder loan_installment = new StringBuilder(serverResponse);
+                   
+                   while ((serverResponse = serverIn.readLine()) !=null && !serverResponse.isEmpty()){
+                   loan_installment.append("\n").append(serverResponse);
+                }
+                   System.out.println("Server response:\n" + loan_installment.toString());
+                          
                 } else {
                     System.out.println("Server response: " + serverResponse);
-                    
-                     if (serverResponse.contains("Do you accept the loan ")) {
-            String userResponse = in.readLine();
-            out.println(userResponse); 
-            
-                }
                     System.out.println(Client.displayMenu());
                 }
-
+            }
+                 
+                 System.out.println(Client.displayMenu());
             }
 
             in.close();
